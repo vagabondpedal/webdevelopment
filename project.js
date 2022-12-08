0c7c9292c51c0e1b4a127156805fb6c044d294a4
vagabondpedal <119935927+vagabondpedal@users.noreply.github.com>
2022-12-08 19:04:21 +0530

    Add files via upload

---
+++ project.js
@@ -0,0 +1,11 @@
+let b =document.getElementById("click")
+b.addEventListener('mouseenter',function()
+{
+    b.style.backgroundColor="yellow";
+    b.style.color='green';
+    
+    window.location.assign("https://github.com/vagabondpedal");
+     
+   
+}
+);
